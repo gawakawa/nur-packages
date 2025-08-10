131209d2c68be8194911886746b285fd5b260c82
gawakawa <mashurakiryu@icloud.com>
2025-08-10 20:56:21 +0900

    :poop: Comment out purescript package

--- default.nix
+++ default.nix
@@ -20,7 +20,7 @@
   go = pkgs.callPackage ./pkgs/go { };
   idris2 = pkgs.callPackage ./pkgs/idris2 { };
   lean = pkgs.callPackage ./pkgs/lean { };
-  purescript = pkgs.callPackage ./pkgs/purescript { };
+  # purescript = pkgs.callPackage ./pkgs/purescript { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
