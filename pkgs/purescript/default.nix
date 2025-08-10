6a8eb3961c3257df2c4ba32345880da16ccd83bf
gawakawa <mashurakiryu@icloud.com>
2025-08-10 20:46:11 +0900

    :bug: Use fetchFromGitHub instead of fetchTarball

--- pkgs/purescript/default.nix
+++ pkgs/purescript/default.nix
@@ -1,8 +1,10 @@
 { pkgs }:
 
 let
-  purescript-overlay = builtins.fetchTarball {
-    url = "https://github.com/thomashoneyman/purescript-overlay/archive/main.tar.gz";
+  purescript-overlay = pkgs.fetchFromGitHub {
+    owner = "thomashoneyman";
+    repo = "purescript-overlay";
+    rev = "main";
     sha256 = "1njsdyhlvsq3l9hwjppbvr4b0l9l7fid45f0f5anisc35jn9xaxa";
   };
   pkgsWithOverlay = import pkgs.path {
